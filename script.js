f1de2154f9e4d8108025b54cc717c3c269242e9e
Gabriela Alves <gabriela_alves1105@hotmail.com>
2022-09-23 21:41:25 -0300

    botão de somar

---
+++ script.js
@@ -0,0 +1,22 @@
+function alertar(){
+    window.alert('Hello, World!')
+}
+
+function mostrarSaudacao() {
+    let nome = window.prompt('Qual é seu nome?');
+    let idade = window.prompt('Quantos anos você tem?');
+    let altura = window.prompt('Qual é sua atura?');
+
+    // let nome = document.getElementById('nome').value;
+
+    console.log(`Olá ${nome}, você tem ${idade} anos e ${altura} de altura`)
+
+    // console.log('Olá', nome, ', você tem', idade, 'anos e', altura,'de altura');
+    //  window.alert('Olá', nome, ', você tem', idade, 'anos e', altura,'de altura');
+    // window.alert(`Olá ${nome}`);
+}
+// mostrarSaudacao();
+function somar(){
+    let valor1 = window.prompt('Digite o valor 1');
+    let valor2 = window.prompt('Digite o valor 2');
+}
